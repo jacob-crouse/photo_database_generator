8faa7325e0cef3f9aa4f4f5352a115c3c437ebbe
Jacob R Crouse <jrc665@psu.edu>
2019-08-12 14:16:42 -0400

    Fixed bug where foregrounds weren't being resized to the correct dimensions.

--- photo_generator.py
+++ photo_generator.py
@@ -2,7 +2,7 @@
 # Photo Database Generator                                       #
 # Author: Jacob Crouse                                           #
 # Date Created: August 6th, 2019                                 #
-# Date Modified: August 11th, 2019                               #
+# Date Modified: August 12th, 2019                               #
 # Purpose: This program is for randomly creating superimposed    #
 #          images that will be used to train neural networks     #
 #          for target recognition and object detection.          #
@@ -11,26 +11,15 @@ from PIL import Image
 import numpy as np
 import os
 
-#define constant variables
-numPhotosToGeneratePerBackground = int(input("How many photos would you like per background?: "))
-numBackgrounds = 4
-numForegrounds = 3
-totalPhotosToGenerate = numPhotosToGeneratePerBackground * numBackgrounds #number of photos to be generated
-background_dims = (60.50, 80.56) #m, grounddimensions of the image from 25m above the ground
-boxDims_red = (0.75, 0.65) #m, dimensions of the post box
-boxDims_blue = (1.2, 1.2) #m, dimensions of the post box
-boxDims_yellow = (1.5, 1.5) #m, dimensions of the post box
-box_physical_dims = (boxDims_red, boxDims_blue, boxDims_yellow)
-
 #calculate cropped sizes for each foreground image
-def calcSizes(box_physical_dims, fore_dims, bg_dims, numBackgrounds, numForegrounds): #[rows][columns]
+def calcSizes(box_physical_dims, fore_dims, bg_images, numBackgrounds, numForegrounds): #[rows][columns]
     crop_sizes = []
 
     for bg in range(numBackgrounds): #for each background
         crop_sizes += [[]]
         for fore in range(numForegrounds): #and for each foreground, calculate the cropped image dimensions
-                xdim = np.floor(box_physical_dims[fore][0] / background_dims[0] * bg_dims[bg][0])
-                ydim = np.floor(box_physical_dims[fore][1] / background_dims[1] * bg_dims[bg][1])
+                xdim = np.floor(box_physical_dims[fore][0] / bg_physical_dims[0] * bg_images[bg][0].size[0])
+                ydim = np.floor(box_physical_dims[fore][1] / bg_physical_dims[1] * bg_images[bg][0].size[1])
                 crop_sizes[bg].append([xdim, ydim])
 
     return crop_sizes
@@ -48,13 +37,15 @@ def genCroppedImages(numBackgrounds, numForegrounds, fore_references): #rows = b
     return cropped_fores
 
 #for each background, create one copy per foreground
-def genBackgroundCopies(numBackgrounds, numForegrounds, bg_references, totalPhotosToGenerate): #rows = bg, cols = fore
+def genBackgroundCopies(numBackgrounds, numForegrounds, bg_references, totalPhotosToGenerate, originalPhotoAspectRatio): #rows = bg, cols = fore
     bg_copies = []
 
     for bg in range(numBackgrounds):
         bg_copies += [[]]
         for fore in range(int(np.floor(totalPhotosToGenerate/numBackgrounds))):
-            bg_copies[bg].append(bg_references[bg].copy())
+            AR = [bg_references[bg].size[0], bg_references[bg].size[0]*(originalPhotoAspectRatio[1]/originalPhotoAspectRatio[0])]
+            
+            bg_copies[bg].append(bg_references[bg].copy().crop([0,0,AR[0],AR[1]]))
 
     return bg_copies
 
@@ -118,9 +109,21 @@ def superimpose(bg_copies, cropped_fores,totalPhotosToGenerate):
     os.close(fd)
     os.chdir("..")
 
+
+
+#define constant variables
+numPhotosToGeneratePerBackground = int(input("How many photos would you like per background?: "))
+originalPhotoAspectRatio = [4,3] #the original aspect ratio of the camera for the background images
+bg_physical_dims = (80.56, 60.5) #m, ground dimensions of the image from 25m above the ground
+boxDims_red = (0.75, 0.65) #m, dimensions of the post box
+boxDims_blue = (1.2, 1.2) #m, dimensions of the post box
+boxDims_yellow = (1.5, 1.5) #m, dimensions of the post box
+box_physical_dims = (boxDims_red, boxDims_blue, boxDims_yellow)
+
 #import all the foregrounds
 os.chdir("foregrounds")
 foreground_files = os.listdir(".")
+numForegrounds = len(foreground_files)
 fore_references = []
 fore_dims = []
 counter = 0
@@ -132,24 +135,25 @@ for file in foreground_files:
 #import all the backgrounds
 os.chdir("../backgrounds")
 background_files = os.listdir(".")
+numBackgrounds = len(background_files)
 bg_references = []
-bg_dims = []
 counter = 0
 for file in background_files:
     bg_references += [Image.open(file)]
-    bg_dims += [bg_references[counter].size]
     counter += 1
 
 os.chdir("..")
 
+totalPhotosToGenerate = numPhotosToGeneratePerBackground * numBackgrounds #number of photos to be generated
+
+#generate the right amount of copies of the background images
+bg_copies = genBackgroundCopies(numBackgrounds, numForegrounds, bg_references, totalPhotosToGenerate, originalPhotoAspectRatio)
+
 #calculate the different sizes for the fore images
-crop_sizes = calcSizes(box_physical_dims, fore_dims, bg_dims, numBackgrounds, numForegrounds)
+crop_sizes = calcSizes(box_physical_dims, fore_dims, bg_copies, numBackgrounds, numForegrounds)
 
 #generate the cropped images
 cropped_fores = genCroppedImages(numBackgrounds, numForegrounds, fore_references)
-
-#generate the right amount of copies of the background images
-bg_copies = genBackgroundCopies(numBackgrounds, numForegrounds, bg_references, totalPhotosToGenerate)
 
 #superimpose each foreground onto each background, save the images
 superimpose(bg_copies, cropped_fores, totalPhotosToGenerate)
